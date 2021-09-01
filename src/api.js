fb5d3f758175202ab7ba76c92d5ab79960cb4680
Vishal Kumar <43844177+vishalkss@users.noreply.github.com>
2021-09-01 11:14:45 +0530

    /search-user updated

--- src/api.js
+++ src/api.js
@@ -303,7 +303,8 @@ async function authenticatUserToken(mobile_number, jwt_token) {
 
 
 //API to search other users in system based on their name | contact
-app.post("/search-user", async (req, res) => {
+// app.post("/search-user", async (req, res) => {
+router.get("/search-user", (req, res) => {
   var search_input = req.param("search_input"); //required field
   // Validate user input
   if (!(search_input)) {
