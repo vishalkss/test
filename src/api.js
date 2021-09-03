838d9637e23b4eecb886e57d36a9c81c37abfea4
Vishal Kumar <43844177+vishalkss@users.noreply.github.com>
2021-09-03 10:00:18 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -36,16 +36,16 @@ app.listen(port, () => {
 
 // var mongo = require('mongodb');
 
-// var MongoClient = require('mongodb').MongoClient;
-// // var url = "mongodb://localhost:27017/mydb";
+var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
 
-// const url = "mongodb+srv://user:user@cluster0.lschs.mongodb.net/user?retryWrites=true&w=majority";
+const url = "mongodb+srv://user:user@cluster0.lschs.mongodb.net/user?retryWrites=true&w=majority";
 
-// MongoClient.connect(url, function (err, db) {
-//     if (err) throw err;
-//     console.log("Database created!");
-//     db.close();
-// });
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
 
 router.get("/", (req, res) => {
   res.json({
